b8de09017e5c08c0083a56f152a0b3630d668fb6
Vanessa Stewart <vstewart1213@gmail.com>
2021-01-23 00:03:47 -0800

    Set up socket on client and server

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const socket = require('socket.io');
 
 // App Setup
 const app = express();
@@ -10,3 +11,10 @@ const server = app.listen(4000, () => {
 
 // Static Files
 app.use(express.static('public'));
+
+// Socket Setup
+const io = socket(server);
+
+io.on('connection', (socket) => {   // socket arg is an instance of the socket
+  console.log('made socket connection', socket.id)
+})
---
+++ public/chat.js
@@ -0,0 +1,2 @@
+// Establish connection between client and server
+const socket = io.connect('http://localhost:4000');
